28cf3f4834040993bd00d4ae17ef1499ad27a6bd
Andrew Howard <andrew@adh.me.uk>
2025-12-02 08:50:39 +0000

    Implemented day 2

---
+++ day02/src/part1.rs
@@ -0,0 +1,50 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Error parsing input: {0}")]
+    ParsingError(#[from] std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<usize, Error> {
+    input
+        .split(',')
+        .try_fold(0, |acc, range| Ok(acc + process_range(range)?))
+}
+
+fn process_range(range: &str) -> Result<usize, Error> {
+    let range = range.trim();
+    let (start, end) = range.split_once('-').unwrap();
+    let start: usize = start.parse()?;
+    let end: usize = end.parse()?;
+    Ok((start..=end).filter(invalid_id).sum())
+}
+
+fn invalid_id(id: &usize) -> bool {
+    let digits = id.to_string();
+    let mid = digits.len() / 2;
+    let (left, right) = digits.split_at(mid);
+    left == right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn example() {
+        let result = solve(EXAMPLE).unwrap();
+        assert_eq!(result, 1227775554);
+    }
+
+    #[cfg(input_txt)]
+    #[cfg(part1_txt)]
+    #[test]
+    fn result() {
+        let expected = include_str!("../part1.txt").trim().parse().unwrap();
+        let result = solve(super::super::INPUT).unwrap();
+        assert_eq!(result, expected);
+    }
+}
---
+++ day02/src/part2.rs
@@ -0,0 +1,59 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Error parsing input: {0}")]
+    ParsingError(#[from] std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<usize, Error> {
+    input
+        .split(',')
+        .try_fold(0, |acc, range| Ok(acc + process_range(range)?))
+}
+
+fn process_range(range: &str) -> Result<usize, Error> {
+    let range = range.trim();
+    let (start, end) = range.split_once('-').unwrap();
+    let start: usize = start.parse()?;
+    let end: usize = end.parse()?;
+    Ok((start..=end).filter(invalid_id).sum())
+}
+
+fn invalid_id(id: &usize) -> bool {
+    let digits = id.to_string();
+    let len = digits.len();
+    for n in 1..=(len / 2) {
+        if len.is_multiple_of(n) && digits_repeat_every_n_characters(&digits, n) {
+            return true;
+        }
+    }
+    false
+}
+
+fn digits_repeat_every_n_characters(digits: &str, n: usize) -> bool {
+    let (a, b) = digits.split_at(n);
+    a == b || (b.starts_with(a) && digits_repeat_every_n_characters(b, n))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn example() {
+        let result = solve(EXAMPLE).unwrap();
+        assert_eq!(result, 4174379265);
+    }
+
+    #[cfg(input_txt)]
+    #[cfg(part2_txt)]
+    #[test]
+    fn result() {
+        let expected = include_str!("../part2.txt").trim().parse().unwrap();
+        let result = solve(super::super::INPUT).unwrap();
+        assert_eq!(result, expected);
+    }
+}
