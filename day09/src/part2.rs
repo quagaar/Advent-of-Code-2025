57cf1db661d0a2185336c71226bfdafff2904f40
Andrew Howard <andrew@adh.me.uk>
2025-12-09 12:17:58 +0000

    Implemented day 9 part 2

--- day09/src/part2.rs
+++ day09/src/part2.rs
@@ -1,6 +1,5 @@
 use glam::U64Vec2;
 use itertools::Itertools;
-use std::cmp::Ordering::{Equal, Greater, Less};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -19,7 +18,7 @@ pub fn solve(input: &str) -> Result<u64, Error> {
         .iter()
         .tuple_combinations()
         .filter(|(p1, p2)| {
-            // Check if there are no other points inside the rectangle formed by p1 and p2
+            // Check if there are no other lines crossing rectangle formed by p1 and p2
             let x_range = if p1.x > p2.x {
                 p2.x + 1..p1.x
             } else {
@@ -30,59 +29,26 @@ pub fn solve(input: &str) -> Result<u64, Error> {
             } else {
                 p1.y + 1..p2.y
             };
-            !coords
-                .iter()
-                .any(|p| x_range.contains(&p.x) && y_range.contains(&p.y))
+            !coords.iter().circular_tuple_windows().any(|(a, b)| {
+                // Check if line segment (a, b) crosses the rectangle
+                if a.x == b.x {
+                    // vertical line
+                    x_range.contains(&a.x)
+                        && a.y.min(b.y) < y_range.end
+                        && a.y.max(b.y) >= y_range.start
+                } else {
+                    // horizontal line
+                    y_range.contains(&a.y)
+                        && a.x.min(b.x) < x_range.end
+                        && a.x.max(b.x) >= x_range.start
+                }
+            })
         })
         .map(|(p1, p2)| {
             let w = p1.x.abs_diff(p2.x) + 1;
             let h = p1.y.abs_diff(p2.y) + 1;
             w * h
         })
-        // .circular_tuple_windows()
-        // .map(|(a, b, c)| {
-        //     let convex = match (b.x.cmp(&a.x), b.y.cmp(&a.y), b.x.cmp(&c.x), b.y.cmp(&c.y)) {
-        //         // Convex corners
-        //         (Greater, Equal, Equal, Less) => true,
-        //         (Equal, Greater, Greater, Equal) => true,
-        //         (Less, Equal, Equal, Greater) => true,
-        //         (Equal, Less, Less, Equal) => true,
-        //         // Concave corners
-        //         (Less, Equal, Equal, Less) => false,
-        //         (Equal, Greater, Less, Equal) => false,
-        //         (Greater, Equal, Equal, Greater) => false,
-        //         (Equal, Less, Greater, Equal) => false,
-        //         // Everything else
-        //         _ => panic!("Points are not forming a corner"),
-        //     };
-        //     (*b, convex)
-        // })
-        // .circular_tuple_windows()
-        // .filter(|((p1, _), (_, convex), (p3, _))| {
-        //     if *convex {
-        //         // Check if there are no other points inside the rectangle formed by p1 and p3
-        //         let x_range = if p1.x > p3.x {
-        //             p3.x + 1..p1.x
-        //         } else {
-        //             p1.x + 1..p3.x
-        //         };
-        //         let y_range = if p1.y > p3.y {
-        //             p3.y + 1..p1.y
-        //         } else {
-        //             p1.y + 1..p3.y
-        //         };
-        //         !coords
-        //             .iter()
-        //             .any(|p| x_range.contains(&p.x) && y_range.contains(&p.y))
-        //     } else {
-        //         false
-        //     }
-        // })
-        // .map(|((p1, _), _, (p3, _))| {
-        //     let w = p1.x.abs_diff(p3.x) + 1;
-        //     let h = p1.y.abs_diff(p3.y) + 1;
-        //     w * h
-        // })
         .max()
         .ok_or(Error::NotFound)
 }
