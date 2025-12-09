95bf90b41834a3324c0cc390078c1eb1bdec1e6a
Andrew Howard <andrew@adh.me.uk>
2025-12-09 11:14:57 +0000

    Day 9 data visualiser

---
+++ day09/src/bin/day09_svg.rs
@@ -0,0 +1,16 @@
+use day09::INPUT;
+
+fn main() {
+    println!(
+        r#"<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="100000" height="100000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<polyline points=""#
+    );
+    print!("{}", INPUT);
+    println!(
+        r#"" stroke="white" stroke-width="400" fill="none" />
+</svg>
+"#
+    );
+}
